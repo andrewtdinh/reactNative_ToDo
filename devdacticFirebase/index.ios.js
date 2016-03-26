eef4373a9fecedd0663e14e6a26455835d6dff94
Andrew Dinh <andrewdinh74@yahoo.com>
2016-03-26 02:15:02 -0700

    Require React and Firebase in index.ios

---
+++ devdacticFirebase/index.ios.js
@@ -0,0 +1,13 @@
+var React = require('react-native');
+var {
+  Component,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput,
+  ListView
+} = React;
+
+var Firebase = require('firebase');
