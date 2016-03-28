de3df7c42c99421d9ad6fb5cf34efeeff1f56612
Andrew Dinh <andrewdinh74@yahoo.com>
2016-03-27 23:52:02 -0700

    Define React class

--- devdacticFirebase/index.ios.js
+++ devdacticFirebase/index.ios.js
@@ -11,3 +11,27 @@ var {
 } = React;
 
 var Firebase = require('firebase');
+
+class devdacticFirebase extends Component {
+  // Your App Code
+  constructor(props) {
+    super(props);
+    var myFirebaseRef = new Firebase('https://.firebaseio.com');
+
+    myFirebaseRef.set({
+      title: "Hello World!",
+      author: "Simon",
+      location: {
+        city: "Muenster",
+        state: "Germany",
+        zip: 48155
+      }
+    });
+  }
+}
+
+var styles = StyleSheet.create({
+  // Your App Style
+});
+
+AppRegistry.registerComponent('devdacticFirebase', () => devdacticFirebase);
